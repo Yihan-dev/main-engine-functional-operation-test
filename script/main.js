3125018f6cb2442bf2efc7ec3b8500a5c5753f0e
남윤엽 <add000775@gmail.com>
2024-02-09 20:40:57 +0900

    격자좌표 반환 기능 구현

--- script/main.js
+++ script/main.js
@@ -2,10 +2,15 @@
 import * as view_model from "./view-model.js";
 
 const stage = document.getElementById('stage');
+const plan_canvas = document.getElementById('plan-layer');
 const tile_canvas = document.getElementById('tile-layer');
-const tile_ctx = tile_canvas.getContext("2d");
 const unit_canvas = document.getElementById('unit-layer');
+const plan_ctx = plan_canvas.getContext("2d");
+const tile_ctx = tile_canvas.getContext("2d");
 const unit_ctx = unit_canvas.getContext("2d");
+
+const 화면타일지름 = 40;
+const 화면타일반지름 = 화면타일지름 / 2;
 
 class 좌표 {
   constructor(x, y) {
@@ -21,9 +26,29 @@ let 이동캔버스좌표 = new 좌표(0, 0);
 let 휠클릭좌표 = new 좌표(0, 0);
 let 마우스좌표 = new 좌표(0, 0);
 let 휠클릭 = false;
-tile_ctx.drawImage(view_model.offscreen_canvas, 0, 0);
+plan_canvas.width = 10000;
+tile_canvas.width = 10000;
+unit_canvas.width = 10000;
+plan_canvas.height = 10000;
+tile_canvas.height = 10000;
+unit_canvas.height = 10000;
+tile_ctx.drawImage(view_model.화면반환(), 0, 0);
 
+view_model.데이터불러오기( [
+  // X, Y,
+  // [타일_높이],
 
+  // [유닛_타일],
+
+  // [유닛_체력],
+  // [유닛_공격력],
+
+  // [유닛_사거리],
+  // [유닛_시야거리],
+  // [유닛_높이],
+
+  // {비활성화_유닛키},
+] );
 
 function mousedown(event) {
   switch (event.button) {
@@ -58,6 +83,12 @@ function mouseup(event) {
 }
 function mousemove(event) {
   마우스좌표 = new 좌표(event.clientX, event.clientY);
+
+  if (event.target.id == 'plan-layer') {
+    const 좌표Y위치 = Math.floor( event.offsetY / 화면타일지름 );
+    const 좌표X위치 = Math.floor( ( event.offsetX - ( (좌표Y위치 & 1) * 화면타일반지름 ) ) / 화면타일지름 );
+  }
+
   if (휠클릭) {
     이동캔버스좌표.x = 기준캔버스좌표.x - 휠클릭좌표.x + 마우스좌표.x;
     이동캔버스좌표.y = 기준캔버스좌표.y - 휠클릭좌표.y + 마우스좌표.y;
--- script/view-model.js
+++ script/view-model.js
@@ -1,14 +1,31 @@
 // 저작권 및 라이센스 표시 공간
 import * as model from "./model.js";
 
-export const offscreen_canvas = document.createElement("canvas");
-offscreen_canvas.height = 1000;
-offscreen_canvas.width = 1000;
-const offscreen_ctx = offscreen_canvas.getContext("2d");
-offscreen_ctx.fillStyle = 'SaddleBrown';
-offscreen_ctx.fillRect(0, 0, 50, 50);
-// console.log( offscreen_ctx.toDataURL() );
+const 타일지름 = 40;
+const 타일반지름 = 타일지름 / 2;
 
-const 임시게임데이터 = [
+const offscreen_canvas = document.createElement("canvas");
+offscreen_canvas.height = 10000;
+offscreen_canvas.width = 10000;
+const offscreen_ctx = offscreen_canvas.getContext("2d");
+offscreen_ctx.fillStyle = '#959595';
+offscreen_ctx.fillRect(0, 0, 40, 40);
+
+export function 데이터불러오기( 데이터 ) {
+
+}
+export function 화면반환() {
+  return offscreen_canvas
+}
+export function 유닛생성클릭() {
   
-]
+}
+export function 유닛수치변경클릭() {
+  
+}
+export function 타일클릭(x, y) {
+  // n = x + yX
+}
+export function 계획처리클릭() {
+  
+}
